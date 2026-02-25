8eac9d394c4fb542e69b53ed0c5e321309d6abed
viraj naik <virunaik05@gmail.com>
2026-02-25 15:32:29 +0530

    addded ec2

---
+++ Terraform Scripts/Ec2/main.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+    region = "ap-south-1"
+  
+}
+
+resource "aws_instance" "myinstance" {
+    ami = "ami-0b6c6ebed2801a5cb"
+    instance_type = "t3.micro"
+    security_groups = [aws_security_group.myvirusg.id]
+}
+
+resource "aws_security_group" "myvirusg" {
+    name = "http-allow"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  
+}
